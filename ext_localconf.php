e897e58b8ad320f8c21da4b525a94721a13114eb
Christian Wolfram <c.wolfram@chriwo.de>
2014-08-06 17:20:49 +0200

    Remove old internal TYPO3 function and replace them with new one

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,10 @@
 <?php
-  if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-  $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['templavoila']['db_new_content_el']['wizardItemsHook'][] = \t3lib_extMgm::extPath($_EXTKEY).'hook/class.newContentElement_hook.php:&tx_fcecolumn_newContentElement_hook'; 
-  
-  ?>
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['templavoila']['db_new_content_el']['wizardItemsHook'][] =
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'hook/class.newContentElement_hook.php:&tx_fcecolumn_newContentElement_hook';
+
+?>
--- hook/class.newContentElement_hook.php
+++ hook/class.newContentElement_hook.php
@@ -24,29 +24,27 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+
 /**
  * HOOK for db_new_content_el.php
  *
  * Originally based on the templavoila extension by Robert Lemke <robert@typo3.org> and Kasper Skaarhoj <kasper@typo3.com>
  */
-
-require_once(PATH_typo3.'interfaces/interface.cms_newcontentelementwizarditemshook.php');
-
-class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizardsHook {
+class tx_fcecolumn_newContentElement_hook implements \TYPO3\CMS\Backend\Wizard\NewContentElementWizardHookInterface {
 
   public function manipulateWizardItems(&$wizardItems, &$parentObject) {
     global $TCA;
     
     // get tv-field
     $colpos = '';
-	  $colAr = \t3lib_div::trimExplode(':',$parentObject->parentRecord,1);
+	  $colAr = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(':',$parentObject->parentRecord,1);
 	  if($pos = array_search('lDEF',$colAr)) {
       if ($pos+1 < count($colAr) && $colAr[$pos+1] != 'vDef') $colpos = $colAr[$pos+1];
     }
       
     // remove fce
     // get remove
-    $TCEFORM_TSconfig = \t3lib_BEfunc::getTCEFORM_TSconfig('tx_fcecolumn', array('pid' => $parentObject->id));
+    $TCEFORM_TSconfig = \TYPO3\CMS\Backend\Utility\BackendUtility::getTCEFORM_TSconfig('tx_fcecolumn', array('pid' => $parentObject->id));
     $tceItems = array();
     if ($colpos != '' && is_array($TCEFORM_TSconfig[$colpos])) {
       foreach ($TCEFORM_TSconfig[$colpos] as $ds => $dsv) {      
@@ -59,7 +57,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
         // Marcus: if onlyItems included it will clear everything and will keep selected items
         if ($dsv['onlyItems']) {
             $removePattern = '*';
-            $onlyItems = \t3lib_div::trimExplode(',',$dsv['onlyItems'],1);
+            $onlyItems = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',',$dsv['onlyItems'],1);
         }
 
         // Marcus: Remove all items with asterrisk or show only selected items
@@ -72,7 +70,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
                  }
             }
         } else {
-            $removeItems = \t3lib_div::trimExplode(',',$removePattern,1);
+            $removeItems = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',',$removePattern,1);
             foreach ($removeItems as $ri) {
               foreach ($wizardItems as $item => $val) {
                  if (is_array($val['tt_content_defValues']) && isset($val['tt_content_defValues'][$dt]) && $val['tt_content_defValues'][$dt]== $ri) {
@@ -84,9 +82,9 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
       }
     }
     
-    \t3lib_div::loadTCA('tt_content');
+    \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('tt_content');
     // Get TCEFORM from TSconfig of current page
-    $TCEFORM_TSconfig = \t3lib_BEfunc::getTCEFORM_TSconfig('tt_content', array('pid' => $parentObject->id));
+    $TCEFORM_TSconfig = \TYPO3\CMS\Backend\Utility\BackendUtility::getTCEFORM_TSconfig('tt_content', array('pid' => $parentObject->id));
     if ($colpos != '') {
         $TCEFORM_TSconfig['CType'] = '';
 
@@ -96,7 +94,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
 
       if (strlen($addvals)>0) {
           if ($onlyTypesItems){
-             $onlyTypesArray = \t3lib_div::trimExplode(',', $onlyTypesItems,1);
+             $onlyTypesArray = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $onlyTypesItems,1);
           }
           
           if ($addvals == '*') {
@@ -120,7 +118,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
     $onlyPluginsPattern = $TCEFORM_TSconfig['tx_fcecolumn'][$colpos.'.']['onlyPlugins'];
     $removePluginsPattern = ($onlyPluginsPattern) ? '*' : $TCEFORM_TSconfig['tx_fcecolumn'][$colpos.'.']['removePlugins'];
 
-    $onlyPlugins = \t3lib_div::trimExplode(',', $onlyPluginsPattern, 1);
+    $onlyPlugins = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $onlyPluginsPattern, 1);
     foreach ($onlyPlugins as $oplgKey => $oplgVal){
         unset($onlyPlugins[$oplgKey]);
         $onlyPlugins[] = 'plugins_'.$oplgVal;
@@ -136,7 +134,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
 
     //    debug($removePlugins);
     } else {
-        $explodedPluginList = \t3lib_div::trimExplode(',', $removePluginsPattern,1);
+        $explodedPluginList = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $removePluginsPattern,1);
         foreach ($explodedPluginList as $rmplVal){
             $removePlugins[] = 'plugins_'.$rmplVal;
         }
@@ -149,7 +147,7 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
     
     
 
-    $removeItems = \t3lib_div::trimExplode(',',$TCEFORM_TSconfig['CType']['removeItems'],1);
+    $removeItems = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',',$TCEFORM_TSconfig['CType']['removeItems'],1);
 
 	$headersUsed = Array();
 			// Traverse wizard items:
@@ -157,12 +155,12 @@ class tx_fcecolumn_newContentElement_hook implements cms_newContentElementWizard
 			// Exploding parameter string, if any (old style)
 		if ($wizardItems[$key]['params'])	{
 				// Explode GET vars recursively
-			$tempGetVars = \t3lib_div::explodeUrl2Array($wizardItems[$key]['params'],TRUE);
+			$tempGetVars = \TYPO3\CMS\Core\Utility\GeneralUtility::explodeUrl2Array($wizardItems[$key]['params'],TRUE);
 				// If tt_content values are set, merge them into the tt_content_defValues array, unset them from $tempGetVars and re-implode $tempGetVars into the param string (in case remaining parameters are around).
 			if (is_array($tempGetVars['defVals']['tt_content']))	{
 				$wizardItems[$key]['tt_content_defValues'] = array_merge(is_array($wizardItems[$key]['tt_content_defValues']) ? $wizardItems[$key]['tt_content_defValues'] : array(), $tempGetVars['defVals']['tt_content']);
 				unset($tempGetVars['defVals']['tt_content']);
-				$wizardItems[$key]['params'] = t3lib_div::implodeArrayForUrl('',$tempGetVars);
+				$wizardItems[$key]['params'] = \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('',$tempGetVars);
 			}
 		}
 			// If tt_content_defValues are defined...:
